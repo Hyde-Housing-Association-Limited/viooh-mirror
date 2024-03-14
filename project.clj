ea3fef038f2895f1b5d1e35ec1674f65fb60dac4
abhijitpatilglobant <abhijit.patil@globant.com>
2024-03-14 21:49:03 +0000

    Upgraded clojure, mulog and other lib

--- project.clj
+++ project.clj
@@ -8,19 +8,24 @@
 
   :repositories [["confluent" {:url "https://packages.confluent.io/maven/"}]]
 
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.2"]
 
                  [samsara/trackit-core "0.9.3"]
                  [integrant "0.8.0"]
-                 [http-kit "2.5.3"]
+                 [http-kit "2.6.0"]
                  [cheshire "5.10.0"]
                  [metosin/compojure-api "1.1.13"]
                  [slingshot "0.12.2"]
 
                  [com.brunobonacci/safely "0.7.0-alpha3"]
-                 [com.brunobonacci/oneconfig "0.21.0"
-                  :exclusions [samsara/trackit-core com.fasterxml.jackson.core/jackson-databind]]
+                 [com.brunobonacci/oneconfig "0.22.0"
+                  :exclusions [samsara/trackit-core com.fasterxml.jackson.core/jackson-databind
+                               clj-commons/clj-yaml]]
                  [com.fasterxml.jackson.core/jackson-databind "2.10.2"]
+
+                 [clj-commons/clj-yaml               "1.0.27"]
+                 [org.json/json "20231013"]
+                 [org.apache.avro/avro   "1.11.3"]
 
                  [com.viooh/kafka-ssl-helper "0.9.0"]
 
@@ -30,17 +35,17 @@
 
                  ;;logging
                  [org.clojure/tools.logging "1.1.0"]
-                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [ch.qos.logback/logback-classic "1.4.12"]
                  [org.codehaus.janino/janino "3.1.3"] ;; logback configuration conditionals :(
                  [com.internetitem/logback-elasticsearch-appender "1.6"]
 
                  ;; observability
-                 [com.brunobonacci/mulog               "0.7.1"]
-                 [com.brunobonacci/mulog-elasticsearch "0.7.1"]
-                 [com.brunobonacci/mulog-kafka         "0.7.1"]
-                 [com.brunobonacci/mulog-cloudwatch    "0.7.1"]
-                 [com.brunobonacci/mulog-mbean-sampler "0.7.1"]
-                 [com.brunobonacci/mulog-jvm-metrics   "0.7.1"]]
+                 [com.brunobonacci/mulog               "0.8.2"]
+                 [com.brunobonacci/mulog-elasticsearch "0.8.2"]
+                 [com.brunobonacci/mulog-kafka         "0.8.2"]
+                 [com.brunobonacci/mulog-cloudwatch    "0.8.2"]
+                 [com.brunobonacci/mulog-mbean-sampler "0.8.2"]
+                 [com.brunobonacci/mulog-jvm-metrics   "0.8.2"]]
 
   :main viooh.mirror.main
 
